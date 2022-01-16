9e988dd228484318b144b93bc489d7f953958df7
agimz0330 <50951094+agimz0330@users.noreply.github.com>
2022-01-16 22:57:46 +0800

    test record loadmore

--- sonar-master/myRecord.js
+++ sonar-master/myRecord.js
@@ -106,6 +106,59 @@ function initial(){
 
 $("#recordLoadmore").click(function (){
     loadTimes++;
+    let cmd= {"act": "getRecord", "mId": mId, "loadTimes": loadTimes};
+    $.post("myrecords.php", cmd, function (data){
+        data= JSON.parse(data);
+        if(data.status== true){
+            $("#errorMsg").html("");
+    
+            let recordPost= data.recordPost;
+            // for start: (每則貼文)
+            for(var i= 0; i< recordPost.length; i++){
+                var onePost= "<div class=\"col-12 col-sm-6 col-lg-3 single_gallery_item wow fadeInUpBig\" data-wow-delay=\"300ms\">"+
+                                "<div class=\"blog-post-thumbnail\">"+
+                                    "<a class=\"gallery-img\" href=\"";
+                onePost+= recordPost[i].foodImg+ "\"><img src=\""+ recordPost[i].foodImg; // 餐點圖片?????
+                onePost+= "\"></a><div class=\"post-date\"><a href=\"#\">";
+                onePost+= recordPost[i].mealDate; // 日期(Jan 01 '09)(月 日 '年)
+                onePost+= "</a></div> <a href=\"javascript: void(0)\" class=\"edit-btn\"><i class=\"bi bi-trash\"></i></a> </div><div class=\"gallery-content\"><h6><span style=\"color:gold\">";
+                
+                for(var point= 0; point< recordPost[i].foodPoint; point++) // 評價(1~5)
+                    onePost+= "<i class=\"fa fa-star\"></i>&nbsp;"; // 星星
+                
+                onePost+= "</span></h6><h4>"+ recordPost[i].shopName+ " <strong>"; // 店名
+                onePost+= recordPost[i].foodName+ "</strong></h4><h6>"; // 餐點名
+                onePost+= recordPost[i].foodCount+ "份&nbsp;"; // 數量
+                onePost+= "<i class=\"fa fa-dollar\" style=\"color:darkgoldenrod\">"+ recordPost[i].foodCost+ "元</i>&nbsp;"; // 金額
+                onePost+= "<i class=\"fa fa-fire\" style=\"color:darkred\">"+ recordPost[i].foodCal+ "cal </i>&nbsp;&nbsp;"; // 熱量
+                
+                switch(recordPost[i].mealTime){ // 時段
+                    case 'b':
+                    onePost+= "<i class=\"bi bi-brightness-alt-high\">Breakfast</i></h6><p>";
+                    break;
+                    case 'l':
+                    onePost+= "<i class=\"bi bi-brightness-high\">Lunch</i></h6><p>";
+                    break;
+                    case 'd':
+                    onePost+= "<i class=\"bi bi-cloud-moon\">Dinner</i></h6><p>";
+                    break;
+                    case 'n':
+                    onePost+= "<i class=\"bi bi-moon\">Night Meal</i></h6><p>";
+                    break;
+                    case 'o':
+                    onePost+= "<i class=\"bi bi-emoji-wink\">Other</i></h6><p>";
+                    break;
+                }
+                onePost+= recordPost[i].foodNote+ "</p></div></div>"; // 備註
+    
+                $(".sonar-portfolio").append(onePost).isotope("appended", onePost).isotope("destroy");
+            }
+            // for end.
+        }
+        else{
+            $("#errorMsg").html(data.info);
+        }
+    });
     /* 
     ***********************************************************
     (post)
@@ -143,55 +196,6 @@ $("#recordLoadmore").click(function (){
         ]
     };
 
-    if(data.status== true){
-        $("#errorMsg").html("");
-
-        let recordPost= data.recordPost;
-        // for start: (每則貼文)
-        for(var i= 0; i< recordPost.length; i++){
-            var onePost= "<div class=\"col-12 col-sm-6 col-lg-3 single_gallery_item wow fadeInUpBig\" data-wow-delay=\"300ms\">"+
-                            "<div class=\"blog-post-thumbnail\">"+
-                                "<a class=\"gallery-img\" href=\"";
-            onePost+= recordPost[i].foodImg+ "\"><img src=\""+ recordPost[i].foodImg; // 餐點圖片?????
-            onePost+= "\"></a><div class=\"post-date\"><a href=\"#\">";
-            onePost+= recordPost[i].mealDate; // 日期(Jan 01 '09)(月 日 '年)
-            onePost+= "</a></div> <a href=\"javascript: void(0)\" class=\"edit-btn\"><i class=\"bi bi-trash\"></i></a> </div><div class=\"gallery-content\"><h6><span style=\"color:gold\">";
-            
-            for(var point= 0; point< recordPost[i].foodPoint; point++) // 評價(1~5)
-                onePost+= "<i class=\"fa fa-star\"></i>&nbsp;"; // 星星
-            
-            onePost+= "</span></h6><h4>"+ recordPost[i].shopName+ " <strong>"; // 店名
-            onePost+= recordPost[i].foodName+ "</strong></h4><h6>"; // 餐點名
-            onePost+= recordPost[i].foodCount+ "份&nbsp;"; // 數量
-            onePost+= "<i class=\"fa fa-dollar\" style=\"color:darkgoldenrod\">"+ recordPost[i].foodCost+ "元</i>&nbsp;"; // 金額
-            onePost+= "<i class=\"fa fa-fire\" style=\"color:darkred\">"+ recordPost[i].foodCal+ "cal </i>&nbsp;&nbsp;"; // 熱量
-            
-            switch(recordPost[i].mealTime){ // 時段
-                case 'b':
-                onePost+= "<i class=\"bi bi-brightness-alt-high\">Breakfast</i></h6><p>";
-                break;
-                case 'l':
-                onePost+= "<i class=\"bi bi-brightness-high\">Lunch</i></h6><p>";
-                break;
-                case 'd':
-                onePost+= "<i class=\"bi bi-cloud-moon\">Dinner</i></h6><p>";
-                break;
-                case 'n':
-                onePost+= "<i class=\"bi bi-moon\">Night Meal</i></h6><p>";
-                break;
-                case 'o':
-                onePost+= "<i class=\"bi bi-emoji-wink\">Other</i></h6><p>";
-                break;
-            }
-            onePost+= recordPost[i].foodNote+ "</p></div></div>"; // 備註
-
-            $(".sonar-portfolio").append(onePost).isotope("appended", onePost).isotope("destroy");
-        }
-        // for end.
-    }
-    else{
-        $("#errorMsg").html(data.info);
-    }
 });
 
 $("#projects").on("click", ".edit-btn", function(e){ // 刪除一筆紀錄

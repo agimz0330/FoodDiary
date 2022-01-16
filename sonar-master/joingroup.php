3df1f47fb508a451f819813dcb684937f9d833c7
hungchun000 <tt5432014@gmail.com>
2022-01-16 17:51:40 +0800

    joingroup.php and mygroupList.php

--- sonar-master/joingroup.php
+++ sonar-master/joingroup.php
@@ -1,15 +1,11 @@
 <?php
-//notYet
-//joingroup //mygroup //groupmemberDatalist   //allgrouplist        ////印評分單
-
 
 $mId = $_POST['mId'];
 $gname = $_POST['groupName'];
-// $mId ='fd000010';
-// $gname = '海大212';
+// $mId ='fd000012';
+// $gname = '海大203';
 
-
-
+///conn
 require_once 'server.php';
 
 $db = mysqli_connect($serverName, $userName, $password, $databaseName);
@@ -18,27 +14,63 @@ printf("Connect failed:") . mysqli_connect_error();
 exit();
 }
 mysqli_set_charset($db, "utf8");
+////////////////////////////////////////////////////////
+///conn end
 
+//搜尋群組名稱是否存在
+$q = mysqli_query($db,"SELECT groupId FROM mygroup WHERE gName='$gname'");
+if(mysqli_num_rows($q) == 1){
+    //group exists//////
+    $arr = array(
+        'status' => true,
+        'msg' => 'group exists' ,
+    );
 
+    //get groupId
+    $row = mysqli_fetch_array($q);
+    $gIdd = $row['groupId'];
+    // print_r($gIdd);
+    
+    //check member
+    $qq = mysqli_query($db,"SELECT * FROM joingroup WHERE groupId='$gIdd' AND mId='$mId' ");
+    if(mysqli_num_rows($qq)<1){
+        
+        $qqq = mysqli_query($db,"INSERT INTO joingroup (groupId,mId) VALUES ('$gIdd','$mId')"); //joinGroup
+        if($qqq){
+            //join success
+            echo "join success!";
+            $arr = array(
+                'status' => true,
+                'msg' => "join success!" ,
+            ); 
+        }else{
+            //join failed
+            echo "join failed!";
+            $arr = array(
+                'status' => false,
+                'msg' => "join failed!" ,
+            ); 
+        }
 
-$result = mysqli_query($db, "SELECT * FROM member");
+     }else{
+        //member already in the group
+        echo "member already in the group";
+        ///msg info
+        $arr = array(
+            'status' => false,
+            'msg' => 'member already exists' ,
+        );
+    }
 
+}else{
+    //group does not exist
+    echo "group does not exist!";
+    $arr = array(
+        'status' => false,
+        'msg' => "group does not exist!" ,
+    );  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 print_r(json_encode($arr));
 // echo json_encode($arr);

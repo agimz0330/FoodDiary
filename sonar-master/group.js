7e04849e51b57f51388692589936aec80d052923
agimz0330 <50951094+agimz0330@users.noreply.github.com>
2022-01-17 12:10:32 +0800

    clear html

--- sonar-master/group.js
+++ sonar-master/group.js
@@ -12,108 +12,108 @@ function initial(){
         sessionStorage.setItem("mId", "fdtest01");
     }
 
-    var data= { // test
-        "status": true, 
-        "msg": "Successfully show home page.",
-        "groupPost": [ // 8格
-            {"foodName": "餐點", 
-            "shopName": "店家", 
-            "mealTime": "b", 
-            "foodCount": 1, 
-            "foodCost": 200, 
-            "foodCal": 1200, 
-            "foodPoint": 3, 
-            "foodNote": "備註備註備註備註備註備註", 
-            "foodImg": "img/portfolio-img/1.jpg", 
-            "mealDate": "2012-07-03"},
-            {"foodName": "餐點2", 
-            "shopName": "店家2", 
-            "mealTime": "d", 
-            "foodCount": 1, 
-            "foodCost": 111, 
-            "foodCal": 2222, 
-            "foodPoint": 5, 
-            "foodNote": "讚", 
-            "foodImg": "img/portfolio-img/2.jpg", 
-            "mealDate": "2022-01-17"}
-            ]
-    };
+    // var data= { // test
+    //     "status": true, 
+    //     "msg": "Successfully show home page.",
+    //     "groupPost": [ // 8格
+    //         {"foodName": "餐點", 
+    //         "shopName": "店家", 
+    //         "mealTime": "b", 
+    //         "foodCount": 1, 
+    //         "foodCost": 200, 
+    //         "foodCal": 1200, 
+    //         "foodPoint": 3, 
+    //         "foodNote": "備註備註備註備註備註備註", 
+    //         "foodImg": "img/portfolio-img/1.jpg", 
+    //         "mealDate": "2012-07-03"},
+    //         {"foodName": "餐點2", 
+    //         "shopName": "店家2", 
+    //         "mealTime": "d", 
+    //         "foodCount": 1, 
+    //         "foodCost": 111, 
+    //         "foodCal": 2222, 
+    //         "foodPoint": 5, 
+    //         "foodNote": "讚", 
+    //         "foodImg": "img/portfolio-img/2.jpg", 
+    //         "mealDate": "2022-01-17"}
+    //         ]
+    // };
 
-    // let cmd= {"act": "groupPost", "mId": mId, "loadTimes": 1};
-    // $.post("myrecords.php", cmd, function (data){
-    //     data= JSON.parse(data);
+    // // let cmd= {"act": "groupPost", "mId": mId, "loadTimes": 1};
+    // // $.post("myrecords.php", cmd, function (data){
+    // //     data= JSON.parse(data);
 
-        if(data.status== true){
-            let groupPost= data.groupPost;
-            console.log(groupPost);
+    //     if(data.status== true){
+    //         let groupPost= data.groupPost;
+    //         console.log(groupPost);
 
-            for(var i= 0; i< groupPost.length; i++){
-                var onePost= "<div class=\"single-blog-area mb-100 wow fadeInUp\" data-wow-delay=\"300ms\">"+
-                        "<div class=\"blog-post-thumbnail\"><img src=\"";
-                onePost+= groupPost[i].foodImg+ "\">";
-                onePost+= "<div class=\"post-date\"><a href=\"javascript: void(0)\">";
+    //         for(var i= 0; i< groupPost.length; i++){
+    //             var onePost= "<div class=\"single-blog-area mb-100 wow fadeInUp\" data-wow-delay=\"300ms\">"+
+    //                     "<div class=\"blog-post-thumbnail\"><img src=\"";
+    //             onePost+= groupPost[i].foodImg+ "\">";
+    //             onePost+= "<div class=\"post-date\"><a href=\"javascript: void(0)\">";
 
-                var mealDateStr= groupPost[i].mealDate; // 2012-07-03
-                var mealDate= "";
-                var monthStr= mealDateStr.substring(5, 7); // 07
-                if(monthStr== "01") mealDate+= "Jan";
-                else if(monthStr== "02") mealDate+= "Feb";
-                else if(monthStr== "03") mealDate+= "Mar";
-                else if(monthStr== "04") mealDate+= "Apr";
-                else if(monthStr== "05") mealDate+= "May";
-                else if(monthStr== "06") mealDate+= "Jun";
-                else if(monthStr== "07") mealDate+= "Jul";
-                else if(monthStr== "08") mealDate+= "Aug";
-                else if(monthStr== "09") mealDate+= "Sep";
-                else if(monthStr== "10") mealDate+= "Oct";
-                else if(monthStr== "11") mealDate+= "Nov";
-                else mealDate+= "Dec";
-                mealDate+= " "+ mealDateStr.substring(8, 10);
-                mealDate+= " \'"+ mealDateStr.substring(2, 4); // '12
+    //             var mealDateStr= groupPost[i].mealDate; // 2012-07-03
+    //             var mealDate= "";
+    //             var monthStr= mealDateStr.substring(5, 7); // 07
+    //             if(monthStr== "01") mealDate+= "Jan";
+    //             else if(monthStr== "02") mealDate+= "Feb";
+    //             else if(monthStr== "03") mealDate+= "Mar";
+    //             else if(monthStr== "04") mealDate+= "Apr";
+    //             else if(monthStr== "05") mealDate+= "May";
+    //             else if(monthStr== "06") mealDate+= "Jun";
+    //             else if(monthStr== "07") mealDate+= "Jul";
+    //             else if(monthStr== "08") mealDate+= "Aug";
+    //             else if(monthStr== "09") mealDate+= "Sep";
+    //             else if(monthStr== "10") mealDate+= "Oct";
+    //             else if(monthStr== "11") mealDate+= "Nov";
+    //             else mealDate+= "Dec";
+    //             mealDate+= " "+ mealDateStr.substring(8, 10);
+    //             mealDate+= " \'"+ mealDateStr.substring(2, 4); // '12
 
-                onePost+= mealDate; // 日期(Jan 01 '09)(月 日 '年)
-                onePost+= "</a></div></div><div class=\"post-content\">"+
-                            "<a href=\"javascript: void(0)\" class=\"headline\">";
-                onePost+= groupPost[i].shopName+ " <strong>"+ groupPost[i].foodName;
+    //             onePost+= mealDate; // 日期(Jan 01 '09)(月 日 '年)
+    //             onePost+= "</a></div></div><div class=\"post-content\">"+
+    //                         "<a href=\"javascript: void(0)\" class=\"headline\">";
+    //             onePost+= groupPost[i].shopName+ " <strong>"+ groupPost[i].foodName;
 
-                onePost+= "</strong></a><div class=\"post-meta\">"+
-                                "<a href=\"javascript: void(0)\" class=\"comments\">By member"+
-                                "</a> | <span style=\"color:gold\">";
+    //             onePost+= "</strong></a><div class=\"post-meta\">"+
+    //                             "<a href=\"javascript: void(0)\" class=\"comments\">By member"+
+    //                             "</a> | <span style=\"color:gold\">";
                 
-                for(var point= 0; point< groupPost[i].foodPoint; point++) // 評價(1~5)
-                    onePost+= "<i class=\"fa fa-star\"></i>&nbsp;"; // 星星
+    //             for(var point= 0; point< groupPost[i].foodPoint; point++) // 評價(1~5)
+    //                 onePost+= "<i class=\"fa fa-star\"></i>&nbsp;"; // 星星
 
-                onePost+= "</span></div><h6>"+ groupPost[i].foodCount+ "份&nbsp;";
-                onePost+= "<i class=\"fa fa-dollar\" style=\"color:darkgoldenrod\">"+ groupPost[i].foodCost+ "元</i>&nbsp;"; // 金額
-                onePost+= "<i class=\"fa fa-fire\" style=\"color:darkred\">"+ groupPost[i].foodCal+ "cal </i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"; // 熱量
+    //             onePost+= "</span></div><h6>"+ groupPost[i].foodCount+ "份&nbsp;";
+    //             onePost+= "<i class=\"fa fa-dollar\" style=\"color:darkgoldenrod\">"+ groupPost[i].foodCost+ "元</i>&nbsp;"; // 金額
+    //             onePost+= "<i class=\"fa fa-fire\" style=\"color:darkred\">"+ groupPost[i].foodCal+ "cal </i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"; // 熱量
                          
-                switch(groupPost[i].mealTime){ // 時段
-                    case 'b':
-                    onePost+= "<i class=\"bi bi-brightness-alt-high\">Breakfast</i></h6><p>";
-                    break;
-                    case 'l':
-                    onePost+= "<i class=\"bi bi-brightness-high\">Lunch</i></h6><p>";
-                    break;
-                    case 'd':
-                    onePost+= "<i class=\"bi bi-cloud-moon\">Dinner</i></h6><p>";
-                    break;
-                    case 'n':
-                    onePost+= "<i class=\"bi bi-moon\">Night Meal</i></h6><p>";
-                    break;
-                    case 'o':
-                    onePost+= "<i class=\"bi bi-emoji-wink\">Other</i></h6><p>";
-                    break;
-                }
-                onePost+= groupPost[i].foodNote+ "</p></div></div>"; // 備註
+    //             switch(groupPost[i].mealTime){ // 時段
+    //                 case 'b':
+    //                 onePost+= "<i class=\"bi bi-brightness-alt-high\">Breakfast</i></h6><p>";
+    //                 break;
+    //                 case 'l':
+    //                 onePost+= "<i class=\"bi bi-brightness-high\">Lunch</i></h6><p>";
+    //                 break;
+    //                 case 'd':
+    //                 onePost+= "<i class=\"bi bi-cloud-moon\">Dinner</i></h6><p>";
+    //                 break;
+    //                 case 'n':
+    //                 onePost+= "<i class=\"bi bi-moon\">Night Meal</i></h6><p>";
+    //                 break;
+    //                 case 'o':
+    //                 onePost+= "<i class=\"bi bi-emoji-wink\">Other</i></h6><p>";
+    //                 break;
+    //             }
+    //             onePost+= groupPost[i].foodNote+ "</p></div></div>"; // 備註
 
-                $("#groupPost").append(onePost);
-            }
-            // for end.
-        }
-        else{
+    //             $("#groupPost").append(onePost);
+    //         }
+    //         // for end.
+    //     }
+    //     else{
 
-        }
-    // });
+    //     }
+    // // });
     /* 
     ***********************************************************
     (post)
